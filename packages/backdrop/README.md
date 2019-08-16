e0bb9740ebf62f29d85de1f35ee8d0bff1f6007f
Yauheni Sakalou <jekasokoloff@gmail.com>
2019-08-16 16:14:33 +0300

    feat: Add backdrop component

---
+++ packages/backdrop/README.md
@@ -0,0 +1,38 @@
+# `@undercoatcss/backdrop`
+
+## Installation
+
+```bash
+npm install @undercoatcss/backdrop
+```
+
+## Initialization
+
+```javascript
+import Backdrop from '@undercoatcss/backdrop';
+```
+
+```css
+@import '@undercoatcss/backdrop/backdrop';
+```
+
+## CSS settings
+
+Helper accepts some css custom properties
+
+| Property | Default value |
+| --- | --- |
+| --backdrop--opacity | 0.5 |
+| --backdrop--background-color | #000000 |
+| --backdrop--transition-duration | 0.5s |
+| --backdrop--transition-timing-function | ease |
+
+## Methods
+
+### show()
+
+`Show backdrop`
+
+### hide()
+
+`Hide backdrop`
---
+++ packages/backdrop/backdrop.js
@@ -0,0 +1,55 @@
+export default new class Backdrop {
+  constructor() {
+    this.backdropSelector = 'backdrop';
+    this.backdropVisibleSelector = 'backdrop_visible';
+    this.htmlScrollbarSelector = 'has-scrollbar';
+    this.htmlNoScrollSelector = 'noscroll';
+  }
+
+  show() {
+    this.addBackdrop();
+    this.fixViewport();
+    this.backdrop.classList.add(this.backdropVisibleSelector);
+  }
+
+  hide() {
+    if (!this.isVisible()) return;
+
+    this.backdrop.classList.remove(this.backdropVisibleSelector);
+    this.unfixViewport();
+  }
+
+  addBackdrop() {
+    if (this.backdrop) return;
+
+    this.backdrop = document.createElement('div');
+    this.backdrop.classList.add(this.backdropSelector);
+    document.body.append(this.backdrop);
+  }
+
+  isVisible() {
+    if (this.backdrop && this.backdrop.matches(`.${this.backdropVisibleSelector}`)) return true;
+    return false;
+  }
+
+  fixViewport() {
+    const scrollY = window.scrollY || document.documentElement.scrollTop;
+
+    document.body.style.marginTop = `-${scrollY}px`;
+
+    if (window.innerWidth > document.documentElement.clientWidth) {
+      document.documentElement.classList.add(this.htmlScrollbarSelector);
+    }
+
+    document.documentElement.classList.add(this.htmlNoScrollSelector);
+  }
+
+  unfixViewport() {
+    const newScrollTop = -document.body.style.marginTop.slice(0, -2);
+
+    document.documentElement.classList.remove(this.htmlScrollbarSelector);
+    document.documentElement.classList.remove(this.htmlNoScrollSelector);
+    document.body.style.marginTop = null;
+    window.scrollTo(null, newScrollTop);
+  }
+}();
